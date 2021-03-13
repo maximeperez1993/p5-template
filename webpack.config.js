9e4e54f2a15baa4cc574563f499fd60b94e325bc
Maxime PEREZ <max.perez1313@gmail.com>
2021-03-13 18:32:57 +0100

    Update template dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,6 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({title: 'TOREPLACE'}),
-        new webpack.HashedModuleIdsPlugin(),
         new CleanWebpackPlugin()
     ],
     devServer: getDevServer()
